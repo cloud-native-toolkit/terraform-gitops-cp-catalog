8355700b99e360430eed0d3184734b6732188052
Sean Sundberg <seansund@us.ibm.com>
2022-01-27 14:54:32 -0600

    Updates workflow to validate metadata
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- variables.tf
+++ variables.tf
@@ -83,4 +83,5 @@ variable "server_name" {
 variable "entitlement_key" {
   type        = string
   description = "The entitlement key used to access the CP4I images in the container registry. Visit https://myibm.ibm.com/products-services/containerlibrary to get the key"
+  sensitive   = true
 }
