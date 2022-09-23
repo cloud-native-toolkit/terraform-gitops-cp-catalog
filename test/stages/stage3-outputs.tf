19031178f288b220b947435b05e5f880742ad2c4
Sean Sundberg <seansund@us.ibm.com>
2022-09-23 00:36:59 -0500

    Updates validation logic
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -1,18 +1,14 @@
 
-resource null_resource write_outputs {
-  provisioner "local-exec" {
-    command = "echo \"$${OUTPUT}\" > gitops-output.json"
+resource local_file write_outputs {
+  filename = "gitops-output.json"
 
-    environment = {
-      OUTPUT = jsonencode({
-        name        = module.gitops_cp_catalogs.name
-        branch      = module.gitops_cp_catalogs.branch
-        namespace   = module.gitops_cp_catalogs.namespace
-        server_name = module.gitops_cp_catalogs.server_name
-        layer       = module.gitops_cp_catalogs.layer
-        layer_dir   = module.gitops_cp_catalogs.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_cp_catalogs.layer == "services" ? "2-services" : "3-applications")
-        type        = module.gitops_cp_catalogs.type
-      })
-    }
-  }
+  content = jsonencode({
+    name        = module.gitops_cp_catalogs.name
+    branch      = module.gitops_cp_catalogs.branch
+    namespace   = module.gitops_cp_catalogs.namespace
+    server_name = module.gitops_cp_catalogs.server_name
+    layer       = module.gitops_cp_catalogs.layer
+    layer_dir   = module.gitops_cp_catalogs.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_cp_catalogs.layer == "services" ? "2-services" : "3-applications")
+    type        = module.gitops_cp_catalogs.type
+  })
 }
